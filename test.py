318e9ed7b5a99ac3c34b6deb52dc1307585a5139
Penny Webb <penelope.e.webb@gmail.com>
2015-11-19 17:15:14 -0500

    adding hw2

---
+++ test.py
@@ -0,0 +1,25 @@
+import logging
+import unittest
+import MC_HW1intermediateV2
+
+
+class TestMediaCloudAPICall (unittest.TestCase):
+	def testMediaCloudAPICall (self):
+		res= MC_HW1intermediateV2.callMediaCloud()
+		assert res!=None 
+
+
+
+#Notes
+
+#logging.basicConfig(filename="sample.log", level=logging.DEBUG)
+
+#logging.debug('This Works')
+#logging.info('So should this')
+#logging.warning('And this, too')
+#logging.error("An error has happened!")
+
+
+
+if __name__ == "__main__":
+    unittest.main()
